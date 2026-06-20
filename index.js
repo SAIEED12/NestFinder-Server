f7e5039708745b58008b91f38d3eaeb97b547bf4
SAIEED12 <abusaieed0@gmail.com>
2026-06-20 10:30:59 +0600

    Add endpoint to insert properties into MongoDB

--- index.js
+++ index.js
@@ -25,6 +25,20 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const db = client.db("nestFinder");
+    const propertiesCollection = db.collection("properties");
+
+
+    app.post('/owner/properties', async(req, res) =>{
+      const data = req.body
+      const result = await propertiesCollection.insertOne(data)
+      res.send(result)
+    })
+
+
+
+
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log(
